f39a60e0acd2ff3d51b4e9253b3ba503ac6b678f
Akus <akosbodor@gmail.com>
2025-07-25 13:13:54 +0200

    add version endpoint to the app

--- sample_dotnet_app/Program.cs
+++ sample_dotnet_app/Program.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from C# microservice!");
+
+// New endpoint to return version
+app.MapGet("/version", () => 
+{
+    var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
+    return $"Version: {version}";
+});
 
 app.Run();
